87b2a5f65cc54466783af2a42944dffc73496af7
Aycha Tammour <aycha.tammour@gmail.com>
2015-08-26 19:55:42 -0400

    fixed a few problems -mostly issues with masked table...

--- dr5_spec_download.py
+++ dr5_spec_download.py
@@ -13,7 +13,7 @@ import os
 
 
 
-def dr5_download(bals, plates_dir, balq_dir):
+def dr5_download(bals, plates_dir):
 
     """download a list of plates from SDSS DR5. Extract specific files with specific MJDs and FiberIDs 
         bals: file with BAL quasars from DR5 that also contains plate, mjd, and fiberid for each row.
@@ -25,14 +25,14 @@ def dr5_download(bals, plates_dir, balq_dir):
         """
     
     os.mkdir(plates_dir)
-    os.mkdir(balq_dir)
 
     catalog= Table.read(bals)
-    all_plates= catalog['plate'][:]
+    catalog['plate'].fill_value= 0000
+    all_plates= catalog['plate'].filled()
     plates= set(all_plates) #select only the unique values
 
     links_list= []
-
+   
     for plate_num in plates:
         links_list.append('http://das.sdss.org/spectro/ss_tar_23/'+str(plate_num).zfill(4)+'.tar.gz')
 
@@ -46,29 +46,43 @@ def dr5_download(bals, plates_dir, balq_dir):
         i+=1
         print "\n Downloaded", i, "of", len(links_list), "files"
 
-        
+
+    return
+    
+def spec_xtract(bals, plate_dir, balq_dir):
+
+    """extract specific files from each plate"""
+
+    os.mkdir(balq_dir)
+    
+    #plates= os.listdir(plate_dir) this is wrong
+    
+    catalog= Table.read(bals)
+    catalog['plate'].fill_value= 0000
+    all_plates= catalog['plate'].filled()
+    plates= set(all_plates)
+    
     for p in plates:
+        if p !=0000:
+            mjd_ls= catalog['mjd'][catalog['plate']== p]
+            fib_ls= catalog['fiberid'][catalog['plate']== p]
         
-        mjd_ls= catalog['mjd'][catalog['plate']== p]
-        fib_ls= catalog['fiberid'][catalog['plate']== p]
+            plate_num= str(p).zfill(4)
+            print "plate", plate_num, "has", len(mjd_ls), "spectra from DR5 BAL catalog"
         
-        plate_num= str(p).zfill(4)
-        print "extracting files from plate", plate_num
-        print "plate", plate_num, "has", len(mjd_ls), "spectra from DR5 BAL catalog"
+            tarball= tarfile.open(plate_dir+"/"+plate_num+".tar.gz", "r")
+            tarball.extractall()
+            tarball.close()
         
-        tarball= tarfile.open(plates_dir+"/"+plate_num+".tar.gz", "r")
-        tarball.extractall()
-        tarball.close()
-        
-        spec_file_names= []
-        for f in range(len(mjd_ls)):
+            spec_file_names= []
+            for f in range(len(mjd_ls)):
 
-            spec_file_names.append(plate_num+"/spSpec/spSpec-"+str(mjd_ls[f])+"-"+plate_num+"-"+str(fib_ls[f]).zfill(3)+".fit")
+                spec_file_names.append(plate_num+"/spSpec/spSpec-"+str(mjd_ls[f])+"-"+plate_num+"-"+str(fib_ls[f]).zfill(3)+".fit")
             
-        for spec_file in spec_file_names:
-            shutil.copy2(spec_file, balq_dir)
+            for spec_file in spec_file_names:
+                shutil.copy2(spec_file, balq_dir)
 
-        subprocess.call(["rm", "-R", plate_num])
+            subprocess.call(["rm", "-R", plate_num])
 
 
     return
