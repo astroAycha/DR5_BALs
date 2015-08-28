4a60223ccab5093640b06d6da1771fb54b0fd24f
Aycha Tammour <aycha.tammour@gmail.com>
2015-08-28 18:14:06 -0400

    had to move a bunch of plates manually. Looks like some plate files from DR5 are corrupt

--- dr5_spec_download.py
+++ dr5_spec_download.py
@@ -15,14 +15,19 @@ import os
 
 #cross match SDSS Quasar data with the DR5 BAL catalog. Then do some work to create a nice catalog file with the stuff i need.
 
-dr5qso= Table.read('dr5qso.fits') #full DR5 quasar catalog from SDSS
-dr5bal= Table.read('dr5BALCat.fits') #DR5 BAL quasars catalog, downloaded from VizieR
+#dr5qso= Table.read('dr5qso.fits') #full DR5 quasar catalog from SDSS
+#dr5bal= Table.read('dr5BALCat.fits') #DR5 BAL quasars catalog, downloaded from VizieR
     
-dr5bal['SDSS'].name= "SDSSName" #change column name
+#dr5bal['SDSS'].name= "SDSSName" #change column name
     
-t= join(dr5qso, dr5bal, keys= 'SDSSName') #cross match using SDSS name
+#t= join(dr5qso, dr5bal, keys= 'SDSSName', join_type= 'right') #cross-match using SDSS name
+
+#ended up doing the cross-matching using TopCat and the RA and DEC.
+
+t= Table.read('myBALs.fits')
 
 t['z_2'].name= 'z'
+t['plate_1'].name= 'plate'
     
 t['BI_Si_'].name= 'BI_SiIV'
 t['BIO_Si_'].name= 'BIO_SiIV'
@@ -61,15 +66,23 @@ t.keep_columns(['SDSSName', 'RA', 'DEC', 'z', 'psfmag_g', 'M_i', \
 
 t['EW_SiIV'].unit= t['EW_CIV'].unit = t['EW_AlIII'].unit= t['EW_MgII'].unit= 'Angstrom'
 
-"""remove row with problem in plate tarball. The download stopped in plate 0357. tarball file seems corrupt.
- for i in range(0, len(t)):
-        if t['plate'][i] == 357:
-            print i #number of row for the corrupt plate.
-    this gave me 5148 for the number of the row
-            """
-#t.remove_row(5148) #maybe not...
+#remove row with problem in plate tarball. The download stopped in plate 0357. tarball file seems corrupt.
+bad_rows=[]
+bad_plates= [357, 673, 674, 675, 676, 677, 678, 679, 680, 681, 682, 683, 684, 685, 686, 687, 688, 690, 691, 692, 693, 694, 695, 696, 697, 698, 699, 700, 701, 702, 703, 704, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 2069, 2074, 2076, 2079, 2185]
 
-t.write('BALCat.fits')
+for i in range(0, len(t)):
+    
+    for j in bad_plates:
+    
+        if t['plate'][i] == j:
+            print i
+            bad_rows.append(i) #number of rows for the corrupt plate.
+
+t.remove_rows(bad_rows)
+
+print len(t)
+
+t.write('myBALCat.fits')
 #t.write('BALCat.csv')
 
 
@@ -120,33 +133,32 @@ def spec_xtract(bals, plate_dir, balq_dir):
     all_plates= catalog['plate']
     plates= set(all_plates) #select only the unique values
     
+    ord_plates= sorted(plates)
     
-    for p in plates:
-        if p !=0000:
-            mjd_ls= catalog['MJD_spec'][catalog['plate']== p]
-            fib_ls= catalog['fiberid'][catalog['plate']== p]
+    i= 0
+    for p in ord_plates:
+        i+=1
+        print i
+        mjd_ls= catalog['MJD_spec'][catalog['plate']== p]
+        fib_ls= catalog['fiberid'][catalog['plate']== p]
         
-            plate_num= str(p).zfill(4)
-            print "plate", plate_num, "has", len(mjd_ls), "spectra in the DR5 BAL catalog"
+        plate_num= str(p).zfill(4)
+        print "plate", plate_num, "has", len(mjd_ls), "spectra in the DR5 BAL catalog"
         
-            tarball= tarfile.open(plate_dir+"/"+plate_num+".tar.gz", "r")
-            tarball.extractall()
-            tarball.close()
+        tarball= tarfile.open(plate_dir+"/"+plate_num+".tar.gz", "r")
+        tarball.extractall()
+        tarball.close()
         
-            spec_file_names= []
-            for f in range(len(mjd_ls)):
+        spec_file_names= []
+        for f in range(len(mjd_ls)):
 
-                spec_file_names.append(plate_num+"/spSpec/spSpec-"+str(mjd_ls[f])+"-"+plate_num+"-"+str(fib_ls[f]).zfill(3)+".fit")
+            spec_file_names.append(plate_num+"/spSpec/spSpec-"+str(mjd_ls[f])+"-"+plate_num+"-"+str(fib_ls[f]).zfill(3)+".fit")
             
-            for spec_file in spec_file_names:
-                shutil.copy2(spec_file, balq_dir)
+        for spec_file in spec_file_names:
+            shutil.copy2(spec_file, balq_dir)
 
-            subprocess.call(["rm", "-R", plate_num])
+        subprocess.call(["rm", "-R", plate_num])
 
 
     return
-
-
-
-
 
