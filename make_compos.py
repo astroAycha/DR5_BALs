e9df6b5cb824a734b4755a06621d8f6e8571976f
Aycha Tammour <aycha.tammour@gmail.com>
2015-10-26 17:03:26 -0400

    tiny change in dir

--- make_compos.py
+++ make_compos.py
@@ -64,7 +64,7 @@ def spec_compos(line, k):
 
     for m,n in zip(range(k), spec_num): #assumes there is a directory called composites in the working directory
     
-        spec_name= "./composites/4features/"+line+"_"+str(k)+"clstr"+str(m+1)+".fits"
+        spec_name= "./composites"+str(k)+"features/"+line+"_"+str(k)+"clstr"+str(m+1)+".fits"
         spec_file= np.vstack((wlen,compos_ls[m], std_ls[m]))
         
         hdu= fits.PrimaryHDU(spec_file)
