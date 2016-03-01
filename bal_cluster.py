721038bc3acf16d315feefd548a6df3b94fd00e7
Labibahx <aycha.tammour@gmail.com>
2016-02-29 19:08:50 -0500

    minor changes to work with the new catalog

--- bal_cluster.py
+++ bal_cluster.py
@@ -13,12 +13,16 @@ def bal_cluster(line, k):
 
     """ unsupervised clustering using KMeans.
     param:
-    data: data table
     line: which line to use for the clustering features
-    k: number of clusters"""
+    k: number of clusters
+    g1= normalized EW, Vmin, Vmax
+    g2= normalized EW, Vmin, dV
     
-    #data= Table.read('myBALCat.fits')
-    data= Table.read('myBALCat_xtra.csv', format= 'ascii.csv')
+    """
+    group = "g2/"
+    
+    data= Table.read('myBALs.fits')
+    #data= Table.read('myBALCat_xtra.csv', format= 'ascii.csv')
 
     #selec the sample: line has an absorption trough: BI0 >0 , S/N >3, and a redshift cutoff to restrict the bandwidth
 
@@ -33,7 +37,7 @@ def bal_cluster(line, k):
         lum= "logF1400"
 
 
-    s= data[(data['BIO_'+line] >0) & (data['SN1700'] >3) & (data['z'] >z1) & (data['z'] <z2) & (data[lum] !=-999)]
+    s= data[(data[line+'-BIO'] >0) & (data['SN1700'] >3) & (data['Z_HW'] >z1) & (data['Z_HW'] <z2)]# & (data[lum] !=-999)]
     
     print "sample has", len(s), "objects"
 
@@ -41,45 +45,53 @@ def bal_cluster(line, k):
     #features
     """
     redshift= s['Z_HW']
-    bi= s['BI_'+line]
-    ew= s['EW_'+line]
-    vmin= s['Vmin_'+line]
-    vmax= s['Vmax_'+line]
-    fdeep= s['f_deep_'+line]
+    bi= s[line+'-BIO']
+    ew= s[line+'-EW']
+    vmin= s[line+'-vmin']
+    vmax= s[line+'-vmax']
+    fdeep= s[line+'-fdeep']
 
     """
-    s['z'].fill_value= -999
+    s['Z_HW'].fill_value= -999
     redshift= s['z'].filled() #redshift
-    s['BI_'+line].fill_value= -999
-    bi= s['BI_'+line].filled() # balnicity: integration 3000-25000
-    s['BIO_'+line].fill_value= -999
-    bi0= s['BIO_'+line].filled() # modified balnicity: integration 0-25000
-    s['EW_'+line].fill_value= -999
-    ew= s['EW_'+line].filled() # restframe absorption EW
-    s['Vmin_'+line].fill_value= -999
-    vmin= s['Vmin_'+line].filled() # minimum velocity
-    s['Vmax_'+line].fill_value= -999
-    vmax= s['Vmax_'+line].filled() # maximum velocity
-    s['f_deep_'+line].fill_value= -999
-    fdeep= s['f_deep_'+line].filled()
+
+    s[line+'-BI'].fill_value= -999
+    bi= s[line+'-BI'].filled() # balnicity: integration 3000-25000
+
+    s[line+'-BIO'].fill_value= -999
+    bi0= s[line+'-BIO'].filled() # modified balnicity: integration 0-25000
+
+    s[line+'-EW'].fill_value= -999
+    ew= s[line+'-EW'].filled() # restframe absorption EW
+
+    s[line+'-vmin'].fill_value= -999
+    vmin= s[line+'-vmin'].filled() # minimum velocity
+
+    s[line+'-vmax'].fill_value= -999
+    vmax= s[line+'-vmax'].filled() # maximum velocity
+
+    s[line+'-fdeep'].fill_value= -999
+    fdeep= s[line+'-fdeep'].filled()
     
     s[lum].fill_value= -999
     cl= s[lum].filled() # Log of 1400 or 2500 monochromatic luminosity
 
-    s['SDSSName'].fill_value= -999
-    names= s['SDSSName'].filled() # SDSS name
+    s['Name'].fill_value= -999
+    names= s['Name'].filled() # SDSS name
+    
 
-    #standardize parameters before using them in clustering
-    f1= (ew - mean(ew))/std(ew)
-    f2= (vmin - mean(vmin))/std(vmin)
-    f3= (vmax - mean(vmax))/std(vmax)
-    f4= (cl - mean(cl))/std(cl)
+    dv= vmax- vmin # delta v
+    
+    #standardize (normalized) parameters before using them in clustering
+    ew_n= (ew - mean(ew))/std(ew)
+    vmin_n= (vmin - mean(vmin))/std(vmin)
+    vmax_n= (vmax - mean(vmax))/std(vmax)
+    dv_n = (dv - mean(dv))/std(dv)
+    
+    #cl_n= (cl - mean(cl))/std(cl)
     
     # list of features to be used in clustering
-    #f= [f1, f2, f3, f4]
-    f= [f1, f2, f3] # three features (without continuum lum)
-
-    #f= [ew, vmin, vmax, fdeep]# , cl]
+    f= [ew_n, vmin_n, dv_n]
 
     qs= np.column_stack(param for param in f) # 2D array to do clustering on
 
@@ -109,7 +121,7 @@ def bal_cluster(line, k):
                      dtype= ('float64', 'float64', 'float64', 'float64', 'int', 'S18', 'float64'))
                      '''
         
-    clstr_tab.write("./clusters/"+str(len(f))+"features/"+clstr_name+"clstrs.fits", format= 'fits')
+    clstr_tab.write("./clusters/"+group+clstr_name+"clstrs.fits", format= 'fits')
     
     
     #clstr_tab.write("./clusters/"+str(len(f))+"features/"+clstr_name+"clstrs.fits", format= 'fits')
